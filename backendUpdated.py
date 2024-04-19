e5a284820f9a26a31bf6915cb1fa0c2536cb5037
Glisk2 <111385258+Glisk2@users.noreply.github.com>
2024-04-19 09:00:42 -0700

    Update backendUpdated.py

--- backendUpdated.py
+++ backendUpdated.py
@@ -1,14 +1,16 @@
 import pandas
 import openpyxl
-from pdf_creator import PdfCreator
+from pypdf import PdfReader, PdfWriter
 
 
 class ExcelDataframe():
     """ Class which creates Dataframe from given file """
 
-    def __init__(self, filename: str)->None:
-        self.filename = filename
-        self.excelData = pandas.read_excel(self.filename)
+    def __init__(self, dataFilename: str)->None:
+        self.dataFilename = dataFilename
+        self.excelData = pandas.read_excel(self.dataFilename)
+
+        self.userSelectedData = []
 
 
     def GetColumnHeaders(self)->list[str]:
@@ -16,3 +18,26 @@ class ExcelDataframe():
         
         excelHeaders = self.excelData.columns
         return excelHeaders
+    
+
+    
+    def BuildBadges(self, userSelectedFields:list[str], pdfFilename: str):
+
+        # Pass in a list from gui 
+        self.userSelectedFields = userSelectedFields
+
+        pdfTemplate = PdfReader(pdfFilename)
+        pdfFilled = PdfWriter()
+
+        pdfFilled.append(pdfTemplate)
+
+        # pdfFilled.update_page_form_field_values(pdfFilled.pages[0],
+        #     {
+
+        #     }                                    
+        # )
+
+    def CreateUserSelectedDataframe(self):
+        """ Build a smaller dataframe from given data """
+    
+        dataList = self.excelData(self.userSelectedFields)
