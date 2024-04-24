8769adc48f8accc854b75726b920843dcd56d9ad
Zack Coleman <zacharycoleman75@gmail.com>
2024-04-24 10:34:07 -0700

    Updated backend & started unittests

--- backendUpdated.py
+++ backendUpdated.py
@@ -9,8 +9,14 @@ class ExcelDataframe():
     def __init__(self, dataFilename: str)->None:
         self.dataFilename = dataFilename
         self.excelData = pandas.read_excel(self.dataFilename)
+        self.excelWedData = pandas.read_excel(self.dataFilename, "Wednesday workshops")
+        self.excelThuData = pandas.read_excel(self.dataFilename, "Thursday workshops")
 
         self.userSelectedData = []
+
+        self.dictWedData = self.CreateWorkshopDictionary(self.excelWedData)
+        self.dictThuData = self.CreateWorkshopDictionary(self.excelThuData)
+
 
 
     def GetColumnHeaders(self)->list[str]:
@@ -41,3 +47,14 @@ class ExcelDataframe():
         """ Build a smaller dataframe from given data """
     
         dataList = self.excelData(self.userSelectedFields)
+
+    def CreateWorkshopDictionary(self, data : pandas.DataFrame) -> dict:
+        columnID = data.columns.get_loc("workshopID")
+        columnRoom = data.columns.get_loc("Room location")
+        columnPresenter = data.columns.get_loc("Presenter")
+        dataframeDictionary = pandas.Series(data['Room location'].values, data['workshopID'].values).to_dict()
+
+        return dataframeDictionary
+
+
+
---
+++ test_backendUpdated.py
@@ -0,0 +1,16 @@
+import unittest
+import backendUpdated
+
+class TestBackendMethods(unittest.TestCase):
+    
+    def testInitialization(self):
+        #class in instantiated
+        #string not provided
+        #file is not an excel/csv file
+        #file does not have 'registrations' tab
+        #file does not have 'Wednesday workshops' tab
+        #file does not have 'Thursday workshops' tab
+
+
+        #self.assertIsInstance()
+        pass
