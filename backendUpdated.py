b470eb056964acc595f08a4b82662ce4950628e1
Zack Coleman <zacharycoleman75@gmail.com>
2024-04-25 14:44:45 -0700

    Working on Unit Testing

--- backendUpdated.py
+++ backendUpdated.py
@@ -7,35 +7,89 @@ class ExcelDataframe():
     """ Class which creates Dataframe from given file """
 
     def __init__(self, dataFilename: str)->None:
+
+        #Need to add error handling for a non excel file path
         self.dataFilename = dataFilename
+
+        self.buildDataFrames()
+
+        #Don't like that parameter requires first item to succeed
+        self.excelWorkshopDict = self.CreateWorkshopDictionary(self.excelWorkshopData)
+        
+
+    def buildDataFrames(self) -> None:
+        """Build dataframe members using excel file provided"""
         self.excelData = pandas.read_excel(self.dataFilename)
-        self.excelWedData = pandas.read_excel(self.dataFilename, "Wednesday workshops")
-        self.excelThuData = pandas.read_excel(self.dataFilename, "Thursday workshops")
 
-        self.userSelectedData = []
-
-        self.dictWedData = self.CreateWorkshopDictionary(self.excelWedData)
-        self.dictThuData = self.CreateWorkshopDictionary(self.excelThuData)
-
+        #Need to add error handling if sheet names are different or if sheets do not exist
+        #Need to verify that all columns are present or throw and error (Workshop ID, Room, & Presenter)
+        excelWedData = pandas.read_excel(self.dataFilename, "Wednesday workshops")
+        excelThuData = pandas.read_excel(self.dataFilename, "Thursday workshops")
+        self.excelWorkshopData = pandas.concat([excelWedData, excelThuData])
 
 
+    # def buildDictionaries(self) -> None:
+    #     self.dictWedData = self.CreateWorkshopDictionary(self.excelWedData)
+    #     self.dictThuData = self.CreateWorkshopDictionary(self.excelThuData)
+
+    
     def GetColumnHeaders(self)->list[str]:
         """ Get header info from Dataframe object """
         
         excelHeaders = self.excelData.columns
         return excelHeaders
     
-
     
     def BuildBadges(self, userSelectedFields:list[str], pdfFilename: str):
 
-        # Pass in a list from gui 
-        self.userSelectedFields = userSelectedFields
+        # # Create smaller DF with user selected columns
+        # self.userSelectedData = self.excelData(userSelectedFields)
 
+        # Create pdf objects (template and new blank doc)
         pdfTemplate = PdfReader(pdfFilename)
         pdfFilled = PdfWriter()
 
+
+        # Set initial variables for looping
+        page = 0
+        count_per_page = 4
         pdfFilled.append(pdfTemplate)
+
+        #Loop through all items in dataframe
+        for index, row in self.excelData.iterrows():
+            
+            # set value for field in pdf (0 - 3) - should reset every 4th value and use page
+            field_index = index - (page * count_per_page)
+
+            # Set pdf fields to values from dataframe (references dict for workshop details)
+            pdfFilled.update_page_form_field_values(
+                pdfFilled.pages[page],
+                {f"name_{field_index}"      : row['badge_name'],
+                 f"college_{field_index}"   : row['institution'],
+                 f"c2r2_{field_index}"      : self.excelWorkshopDict[row['wed_morning'][0]],
+                 f"c3r2_{field_index}"      : self.excelWorkshopDict[row['wed_morning'][1]],
+                 f"c2r3_{field_index}"      : self.excelWorkshopDict[row['wed_afternoon'][0]],
+                 f"c3r3_{field_index}"      : self.excelWorkshopDict[row['wed_afternoon'][1]],
+                 f"c2r4_{field_index}"      : self.excelWorkshopDict[row['thurs_morning'][0]],
+                 f"c3r4_{field_index}"      : self.excelWorkshopDict[row['thurs_morning'][1]],
+                 f"c2r5_{field_index}"      : self.excelWorkshopDict[row['thurs_afternoon'][0]],
+                 f"c3r5_{field_index}"      : self.excelWorkshopDict[row['thurs_afternoon'][1]],
+                },auto_regenerate=True
+                )
+            
+            # if all four blocks on pdf filled out, reset field index and add new page
+            if field_index >= 3:
+                page += 1
+                pdfFilled.append(pdfTemplate)
+
+        # Change to save in same desitination as template with same name & "_complete" appended
+        # Save complete pdf to folder with provided template
+        with open("filled-out.pdf", "wb") as output_stream:
+            pdfFilled.write(output_stream)
+
+
+
+
 
         # pdfFilled.update_page_form_field_values(pdfFilled.pages[0],
         #     {
@@ -43,10 +97,6 @@ class ExcelDataframe():
         #     }                                    
         # )
 
-    def CreateUserSelectedDataframe(self):
-        """ Build a smaller dataframe from given data """
-    
-        dataList = self.excelData(self.userSelectedFields)
 
     def CreateWorkshopDictionary(self, data : pandas.DataFrame) -> dict:
         columnID = data.columns.get_loc("workshopID")
--- test_backendUpdated.py
+++ test_backendUpdated.py
@@ -1,10 +1,18 @@
 import unittest
-import backendUpdated
+from backendUpdated import ExcelDataframe
+import pandas
 
 class TestBackendMethods(unittest.TestCase):
+
+    dfValues = {'col1': ['A'    , 'B'     , 'C'     ],
+                'col2': [11     , 22      , 33      ],
+                'col3': ['apple', 'banana', 'cherry']}
+    dfMock = pandas.DataFrame(dfValues)
     
     def testInitialization(self):
         #class in instantiated
+        self.assertRaises(something, ExcelDataframe.buildDataFrames)
+        EDF = ExcelDataframe('teststring')
         #string not provided
         #file is not an excel/csv file
         #file does not have 'registrations' tab
@@ -14,3 +22,8 @@ class TestBackendMethods(unittest.TestCase):
 
         #self.assertIsInstance()
         pass
+
+    def testBuildDataFrames(self):
+        self.assertIs(ExcelDataframe.buildDataFrames, pandas.DataFrame)
+
+    
