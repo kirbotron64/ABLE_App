ebee37389c4556a6fe93d3f8748b9addf853300a
Zack Coleman <zacharycoleman75@gmail.com>
2024-05-06 16:37:51 -0700

    Error Handling Update

--- backendUpdated.py
+++ backendUpdated.py
@@ -1,7 +1,6 @@
 import pandas
 import openpyxl
 import os
-#from pypdf import PdfReader, PdfWriter
 import PyPDF2
 
 
@@ -16,44 +15,83 @@ class ExcelDataframe():
         self.pdfTemplatePath = ""
         self.excelData = pandas.DataFrame()
         self.excelWorkshopDict = {}
+        self.presenter_index = 0
+        self.location_index = 0
 
 
+    def set_dataFilePath(self, path: str) -> None:
+        """sets file path field for data file"""
+
+        #add handling for none selected
+        if not (path.lower().endswith('xls') or 
+                path.lower().endswith('xlsx') or 
+                path.lower().endswith('xlsm')):
+            raise Exception("Incorrect file type provided")
+        else:
+            self.dataFilePath = path
+            self.buildDataStructures()
+
+    def set_pdfTemplatePath(self, path: str) -> None:
+        """sets file path field for pdf template file"""
+
+        #add handling for none selected
+        if not path.lower().endswith('pdf'):
+            raise Exception("Incorrect file type provided")
+        else:
+            self.pdfTemplatePath = path
+
+    
     def buildDataStructures(self) -> None:
         """Build dataframe members using excel file provided"""
 
         # Need to add error handling for a non excel file path
         # Need to add error handling if sheet names are different or if sheets do not exist
         # Need to verify that all columns are present or throw and error (Workshop ID, Room, & Presenter)
-        self.excelData = pandas.read_excel(self.dataFilePath)
-        excelWedData = pandas.read_excel(self.dataFilePath, "Wednesday workshops")
-        excelThuData = pandas.read_excel(self.dataFilePath, "Thursday workshops")
+
+        # Build dataframes from excel worksheets
+        try:
+            self.excelData = pandas.read_excel(self.dataFilePath, "registrations")
+        except:
+            raise Exception("\'registrations\' worksheet not found in data file")
+        
+        try:
+            excelWedData = pandas.read_excel(self.dataFilePath, "Wednesday workshops")
+        except:
+            raise Exception("\'Wednesday workshops\' worksheet not found in data file")
+        
+        try:
+            excelThuData = pandas.read_excel(self.dataFilePath, "Thursday workshops")
+        except:
+            raise Exception("\'Thursday workshops\' worksheet not found in data file")
+        
+        #if any above are true then exit
+
+        
+        #Verify data existence in workshop dataframes+
+        if not "workshopID" in excelWedData.columns:
+            raise Exception("Column \'workshopID\' not found in the \'Wednesday workshops\' worksheet")
+        if not "Presenter" in excelWedData.columns:
+            raise Exception("Column \'Presenter\' not found in the \'Wednesday workshops\' worksheet")
+        if not "Location" in excelWedData.columns:
+            raise Exception("Column \'Location\' not found in the \'Wednesday workshops\' worksheet")
+        if not "workshopID" in excelThuData.columns:
+            raise Exception("Column \'workshopID\' not found in the \'Thursday workshops\' worksheet")
+        if not "Presenter" in excelThuData.columns:
+            raise Exception("Column \'Presenter\' not found in the \'Thursday workshops\' worksheet")
+        if not "Location" in excelThuData.columns:
+            raise Exception("Column \'Location\' not found in the \'Thursday workshops\' worksheet")
+        
+        #if any above are true raise exit (use variable fed by the above?)
+
+
+        #Concat workshop dataframes, index columns, and create dictionary
         excelWorkshopData = pandas.concat([excelWedData, excelThuData])
-        # self.excelWorkshopDict = pandas.Series([excelWorkshopData['Room location'].values,
-        #                                         excelWorkshopData['Presenter'].values],
-        #                                         excelWorkshopData['workshopID'].values).to_dict()
+        self.presenter_index = excelWorkshopData.columns.get_loc("Presenter") - 1
+        self.location_index = excelWorkshopData.columns.get_loc("Location") - 1
         self.excelWorkshopDict = excelWorkshopData.set_index('workshopID').T.to_dict('list')
 
 
-    # def createWorkshopDictionary(self, data : pandas.DataFrame) -> dict:
-    #     columnID = data.columns.get_loc("workshopID")
-    #     columnRoom = data.columns.get_loc("Room location")
-    #     columnPresenter = data.columns.get_loc("Presenter")
-    #     dataframeDictionary = pandas.Series([data['Room location'].values, data['Presenter'].values], data['workshopID'].values).to_dict()
 
-    #     return dataframeDictionary
-
-
-    # def buildDictionaries(self) -> None:
-    #     self.dictWedData = self.CreateWorkshopDictionary(self.excelWedData)
-    #     self.dictThuData = self.CreateWorkshopDictionary(self.excelThuData)
-
-    
-    # def getColumnHeaders(self)->list[str]:
-    #     """ Get header info from Dataframe object """
-        
-    #     excelHeaders = self.excelData.columns
-    #     return excelHeaders
-    
     
     def buildBadges(self) -> bool:
         """Uses pdf template provided, data sets provided, and selected columns to construct
@@ -92,20 +130,20 @@ class ExcelDataframe():
                 pdfFilled.pages[page],
                 {f"name_{field_index}"      : row['badge_name'],
                  f"college_{field_index}"   : row['institution'],
-                 f"c2r2_{field_index}"      : self.excelWorkshopDict[row['wed_morning']][2],
-                 f"c3r2_{field_index}"      : self.excelWorkshopDict[row['wed_morning']][1],
-                 f"c2r3_{field_index}"      : self.excelWorkshopDict[row['wed_afternoon']][2],
-                 f"c3r3_{field_index}"      : self.excelWorkshopDict[row['wed_afternoon']][1],
-                 f"c2r4_{field_index}"      : self.excelWorkshopDict[row['thurs_morning']][2],
-                 f"c3r4_{field_index}"      : self.excelWorkshopDict[row['thurs_morning']][1],
-                 f"c2r5_{field_index}"      : self.excelWorkshopDict[row['thurs_afternoon']][2],
-                 f"c3r5_{field_index}"      : self.excelWorkshopDict[row['thurs_afternoon']][1],
+                 f"c2r2_{field_index}"      : self.excelWorkshopDict[row['wed_morning']][self.presenter_index],
+                 f"c3r2_{field_index}"      : self.excelWorkshopDict[row['wed_morning']][self.location_index],
+                 f"c2r3_{field_index}"      : self.excelWorkshopDict[row['wed_afternoon']][self.presenter_index],
+                 f"c3r3_{field_index}"      : self.excelWorkshopDict[row['wed_afternoon']][self.location_index],
+                 f"c2r4_{field_index}"      : self.excelWorkshopDict[row['thurs_morning']][self.presenter_index],
+                 f"c3r4_{field_index}"      : self.excelWorkshopDict[row['thurs_morning']][self.location_index],
+                 f"c2r5_{field_index}"      : self.excelWorkshopDict[row['thurs_afternoon']][self.presenter_index],
+                 f"c3r5_{field_index}"      : self.excelWorkshopDict[row['thurs_afternoon']][self.location_index],
                 }
                 )
             
             # if all four blocks on pdf filled out, reset field index and add new page
             if field_index >= 3:
-                self.pdf_suffix_fields(pdfFilled.pages[page], page)
+                self._pdf_suffix_fields(pdfFilled.pages[page], page)
                 pdfFilled.reset_translation(pdfTemplate)
                 page += 1
                 pdfFilled.append(pdfTemplate)
@@ -117,7 +155,8 @@ class ExcelDataframe():
 
         return True
     
-    def pdf_suffix_fields(self, page, sfx):
+    def _pdf_suffix_fields(self, page, sfx):
+        """Adds a provided suffix to all fields on a provided pdf page"""
         fields = page.get('/Annots')
         if fields:
             for field_ref in fields:
--- gui.py
+++ gui.py
@@ -3,31 +3,24 @@ from tkinter import filedialog
 import tkinter.messagebox
 import os
 from backendUpdated import ExcelDataframe
-from enum_filetypes import FileType
+from typing import Literal
 
 class Gui:
     def __init__(self, window):
         self.window = window
         self.window.title("ABLE Badge Creator")
         self.window.geometry("")
-        # self.window.geometry("465x165")
 
         self.backend = ExcelDataframe()
         self.desktop_path = os.path.join(os.path.join(os.path.expanduser('~')), 'Desktop')
-        # self.data_file_name = ""
-        # self.template_file_name = ""
-        self.data_file_name = "C:/Users/zacha/Documents/Repos/ABLE_App/registrations_April_24.xlsx"
-        self.pdf_file_name = "C:/Users/zacha/Documents/Repos/ABLE_App/form_nested.pdf"
+        self.data_file_name = ""
+        self.template_file_name = ""
 
-        # To be included in future feature update
-        ###self.field_names = []
-        ###self.options = self.backend.options
-        ###self.fields_dict = {} # holds combobox objects
 
     def createInterfaceObjects(self):
         #Create buttons and labels
-        self.button_datafile = Button(self.window, text = "Browse for data file", command=lambda:  self.browseFiles(FileType.DATAFILE), height=1)
-        self.button_pdffile = Button(self.window, text = "Browse for pdf template", command=lambda:  self.browseFiles(FileType.PDF), height=1)
+        self.button_datafile = Button(self.window, text = "Browse for data file", command=lambda:  self.setFilePath('excel'), height=1)
+        self.button_pdffile = Button(self.window, text = "Browse for pdf template", command=lambda:  self.setFilePath('pdf'), height=1)
         self.button_create = Button(self.window, text = "Create Badges", command = self.createBadges, width= 15)
         self.label_datafile = Label(self.window, text = "No file selected", wraplength= 500, justify='left')
         self.label_pdffile = Label(self.window, text = "No file selected", wraplength= 500, justify='left')
@@ -49,75 +42,44 @@ class Gui:
 
 
     # FILE BROWSER
-    def browseFiles(self, type):
+    def setFilePath(self, type: Literal['pdf', 'excel']):
         """File browser for gui - requires parameter spicifying what file type should be searched for using the appropirate enum"""
 
+        match type:
+            case 'pdf':
+                ###Talk to Josh about a better way to do this?
+                filename = self.fileBrowser([('PDF Files', '.pdf')])
+                try:
+                    self.backend.set_pdfTemplatePath(filename)
+                except:
+                    tkinter.messagebox.showinfo("Error!","Incorrect filetype chosen. Please try again.")
+                else:
+                    self.pdf_file_name = filename
+                    self.label_pdffile.configure(text = "File Selected: " + filename)
+            case 'excel':
+                filename = self.fileBrowser([('Excel Files', '.xls*')])
+                try:
+                    self.backend.set_dataFilePath(filename)
+                except:
+                    tkinter.messagebox.showinfo("Error!","Incorrect filetype chosen. Please try again.")
+                else:
+                    self.data_file_name = filename
+                    self.label_datafile.configure(text = "File Selected: " + filename)
+            case _:
+                pass
+
+
+    def fileBrowser(self, type: tuple[str, str]) -> str:
         filename = filedialog.askopenfilename(initialdir = self.desktop_path,
                                               title = "Select a File",
-                                              filetypes = type.value)
-        if filename != "":
-            match type:
-                case FileType.PDF:
-                    self.label_pdffile.configure(text = "File Selected: " + filename)
-                    self.pdf_file_name = filename
-                case FileType.DATAFILE:
-                    self.label_datafile.configure(text = "File Selected: " + filename)
-                    self.data_file_name = filename
-                case _:
-                    # Add error handling
-                    pass
-        print(filename)
+                                              filetypes = type)
+        return filename
             
 
     def createBadges(self):
-        # for i in range(len(self.fields_dict)):
-        #     self.field_names.append(self.fields_dict[f"Field_box_{i + 1}"].get())
-        self.backend.dataFilePath = self.data_file_name
-        self.backend.pdfTemplatePath = self.pdf_file_name
-        self.backend.buildDataStructures()
-
-
         if self.backend.buildBadges() == True:
             tkinter.messagebox.showinfo("Badge Creation Status","Badge creation completed!")
             quit()
-
-
-    # def labelFields(self):    
-    #     # NUM FIELDS LABEL & DROPDOWN
-    #     self.num_fields_label = Label(master,
-    #                               text = "Select number of fields:")
-    #     self.num_fields_label.grid(row=2,column=0,padx=2,pady=2)
-
-    #     self.n = IntVar()
-    #     self.num_fields = ttk.Combobox(master,
-    #                                    width = 8,
-    #                                    textvariable = self.n,
-    #                                    justify = "center")
-    #     self.num_fields["values"] = [2,3,4,5]
-    #     self.num_fields.bind("<<ComboboxSelected>>", self.createFields)
-    #     self.num_fields.grid(row=3,column=0,padx=2,pady=2)
-
-
-    #     # FIELD SELECTION DROPDOWNS
-    #     self.field_select_label = Label(master, text="Field Selection:")
-    #     self.field_select_label.grid(row=4,padx=2,pady=2)
-
-
-
-    # def createFields(self, event):
-    #     for i in range(len(self.fields_dict)):
-    #         self.fields_dict[f"Field_box_{i + 1}"].grid_forget()
-    #     self.fields_dict.clear()
-    #     num_fields = int(self.num_fields.get())
-    #     for i in range(num_fields):
-    #         self.field = StringVar()
-    #         box = ttk.Combobox(self.master,
-    #                            width = 15,
-    #                            textvariable = self.field,
-    #                            values = self.options)
-    #         self.fields_dict[f"Field_box_{i + 1}"] = box
-    #     for i in range(len(self.fields_dict)):
-    #         self.fields_dict[f"Field_box_{i + 1}"].grid(row=4,column=i+1,padx=2,pady=2)
 
         
         
